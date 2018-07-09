b91c2ae854f507221730a52dc01e07814a766e75
cateland <catelandaxel@gmail.com>
2018-07-09 17:48:46 +0200

    attempt to simplify code

--- src/Systems/DraggableSystem.elm
+++ src/Systems/DraggableSystem.elm
@@ -16,13 +16,89 @@ onMoveUpdateEntityDragStatus :
 onMoveUpdateEntityDragStatus dragStatus position entity =
     { entity | drag = Just (createDragged (updateCurrentPosition dragStatus position)) }
 
-onMoveUpdateEntityShape
-    : Draggable.DragStatus
+
+onMoveUpdateEntityShape :
+    Draggable.DragStatus
     -> Shape.Shape
     -> Entity
     -> Entity
 onMoveUpdateEntityShape dragStatus shape entity =
     { entity | shape = Just (translateBy (getDragMoveVectorDelta dragStatus) shape) }
+
+
+onMouseMove : Draggable -> Position -> Shape.Shape -> ( Entity, NewEntities ) -> ( Entity, NewEntities )
+onMouseMove dragg position shape tuple =
+    let
+        ( entity, newEntities ) =
+            tuple
+    in
+        case getDraggStatus dragg of
+            Just dragStatus ->
+                let
+                    updatedEntity =
+                        entity
+                            |> onMoveUpdateEntityDragStatus dragStatus position
+                            |> onMoveUpdateEntityShape dragStatus shape
+                in
+                    ( updatedEntity, newEntities )
+
+            Nothing ->
+                tuple
+
+
+onMouseRelease : ( Entity, NewEntities ) -> ( Entity, NewEntities )
+onMouseRelease tuple =
+    let
+        ( entity, newEntities ) =
+            tuple
+    in
+        ( { entity | drag = Just createNotDragged }, newEntities )
+
+
+onMousePress : Position -> Shape.Shape -> ( Entity, NewEntities ) -> ( Entity, NewEntities )
+onMousePress position shape tuple =
+    let
+        ( entity, newEntities ) =
+            tuple
+    in
+        if isVectorOver (positionToPoint2d position) shape then
+            let
+                dragStatus =
+                    { startPos = position
+                    , previousPos = position
+                    , currentPos = position
+                    }
+            in
+                ( { entity | drag = Just (createDragged dragStatus) }, newEntities )
+        else
+            tuple
+
+
+update : ( Entity, NewEntities ) -> Msg -> Draggable -> Shape.Shape -> ( Entity, NewEntities )
+update tuple msg dragg shape =
+    let
+        ( entity, newEntities ) =
+            tuple
+    in
+        case isDragged dragg of
+            True ->
+                case msg of
+                    Msgs.Move position ->
+                        onMouseMove dragg position shape tuple
+
+                    Msgs.Release position ->
+                        onMouseRelease tuple
+
+                    _ ->
+                        tuple
+
+            False ->
+                case msg of
+                    Msgs.Press position ->
+                        onMousePress position shape tuple
+
+                    _ ->
+                        tuple
 
 
 draggableSystem : Msgs.Msg -> Entities -> String -> ( Entity, NewEntities ) -> ( Entity, NewEntities )
@@ -31,48 +107,5 @@ draggableSystem msg entities key tuple =
         ( entity, newEntities ) =
             tuple
     in
-        case ( entity.drag, entity.shape ) of
-            ( Just dragg, Just shape ) ->
-                case isDragged dragg of
-                    True ->
-                        case msg of
-                            Msgs.Move position ->
-                                case getDraggStatus dragg of
-                                    Just dragStatus ->
-                                        let
-                                            updatedEntity =
-                                                entity
-                                                    |> onMoveUpdateEntityDragStatus dragStatus position
-                                                    |> onMoveUpdateEntityShape dragStatus shape
-                                        in
-                                            ( updatedEntity, newEntities )
-
-                                    Nothing ->
-                                        tuple
-
-                            Msgs.Release position ->
-                                ( { entity | drag = Just createNotDragged }, newEntities )
-
-                            _ ->
-                                tuple
-
-                    False ->
-                        case msg of
-                            Msgs.Press position ->
-                                if isVectorOver (positionToPoint2d position) shape then
-                                    let
-                                        dragStatus =
-                                            { startPos = position
-                                            , previousPos = position
-                                            , currentPos = position
-                                            }
-                                    in
-                                        ( { entity | drag = Just (createDragged dragStatus) }, newEntities )
-                                else
-                                    tuple
-
-                            _ ->
-                                tuple
-
-            _ ->
-                tuple
+        Maybe.map2 (update tuple msg) entity.drag entity.shape
+            |> Maybe.withDefault tuple
